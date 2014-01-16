79e6ffb304c499dc06be671d2536e767c0ebdfb4
Diego Ortiz-Monasterio <dortizmonasterio@gmail.com>
2014-01-16 14:44:08 -0700

    Updated Accesss-Control-Allow-Origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :set_access_control_headers
+
+  private
+
+  def set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = "http://198.199.108.233"
+  end
 end
