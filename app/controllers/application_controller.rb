29ef24306f0414b99055e6d16957d829eec62c8f
Diego Ortiz-Monasterio <dortizmonasterio@gmail.com>
2014-01-16 21:23:28 -0700

    Tried to fix CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  after_filter :set_access_control_headers
-
-  private
-
-  def set_access_control_headers
-      headers['Access-Control-Allow-Origin'] = "*"
-  end
+  protect_from_forgery with: :null_session
 end
